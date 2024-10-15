302468cddce961f72d3c05962ef72d945ac8ef87
dvtuan282 <dvtuan282@gmail.com>
2024-10-15 23:29:21 +0700

    create project

---
+++ src/test/java/vn/paso/sendId/tasks/OpenTheApplication.java
@@ -0,0 +1,19 @@
+package vn.paso.sendId.tasks;
+
+import net.serenitybdd.annotations.Step;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Open;
+import vn.paso.sendId.ui.GoogleSearchPage;
+
+public class OpenTheApplication implements Task {
+
+    GoogleSearchPage googleSearchPage;
+
+    @Step("Open the application")
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Open.browserOn().the(googleSearchPage)
+        );
+    }
+}
---
+++ src/test/java/vn/paso/sendId/ui/PhoneNumberPage.java
@@ -0,0 +1,4 @@
+package vn.paso.sendId.ui;
+
+public class PhoneNumberPage {
+}
